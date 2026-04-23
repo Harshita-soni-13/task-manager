c87e1c6a85b5be239068a821ef8a73bcfd2cc658
HarshitaBrandshow <harshita.trainee@brandshow.in>
2026-04-23 12:47:51 +0530

    backend url update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000/api"         // local url 
+   baseURL: "https://task-manager-backend-x0k3.onrender.com"     //render url
 });
 
 // Auto attach token
